3d4a4fbf36cfcfac0c6a99d0f9c424cdd6e4480a
W1ndys <w1ndys@outlook.com>
2025-02-09 17:49:15 +0800

    chore: Update WebSocket API port in local environment configuration

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ owner_id = ["2769731875"]  # 机器人root管理员 QQ 号
 
 report_group_id = "910696002"  # 上报群
 
-ws_url = "ws://127.0.0.1:3000"  # 本地环境的 WebSocket API 地址
+ws_url = "ws://127.0.0.1:3001"  # 本地环境的 WebSocket API 地址
 
 token = None  # 如果需要认证，请填写认证 token
 
