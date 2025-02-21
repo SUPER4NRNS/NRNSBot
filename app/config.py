98b00fd5337ae0ee5a670366f4b5eec68b28a676
SUPER4NRNS <super4nrns@vip.qq.com>
2025-02-22 02:43:45 +0800

    0

--- app/config.py
+++ app/config.py
@@ -1,12 +1,12 @@
 # config.py
 
 owner_id = [
-    "2769731875"
+    "1010332940"
 ]  # 机器人root管理员 QQ 号，可以有多个，多个时，需要用英文逗号分隔，上线通知只会给第一个
 
-report_group_id = "910696002"  # 上报群
+report_group_id = "294824017"  # 上报群
 
-ws_url = "ws://127.0.0.1:3001"  # 本地环境的 WebSocket API 地址
+ws_url = "ws://192.168.3.7:3001"  # 本地环境的 WebSocket API 地址
 
 token = None  # 如果需要认证，请填写认证 token
 
--- app/handler_events.py
+++ app/handler_events.py
@@ -10,11 +10,11 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # 统一从各模块导入事件处理器
 from app.scripts.ImageGenerate.main import handle_events as handle_ImageGenerate_events
-from app.scripts.SendAll.main import handle_events as handle_SendAll_events
+#from app.scripts.SendAll.main import handle_events as handle_SendAll_events
 from app.scripts.GroupManager.main import handle_events as handle_GroupManager_events
-from app.scripts.Crypto.main import handle_events as handle_Crypto_events
+#from app.scripts.Crypto.main import handle_events as handle_Crypto_events
 from app.scripts.QASystem.main import handle_events as handle_QASystem_events
-from app.scripts.ClassTable.main import handle_events as handle_ClassTable_events
+#from app.scripts.ClassTable.main import handle_events as handle_ClassTable_events
 from app.scripts.KeywordsReply.main import handle_events as handle_KeywordsReply_events
 from app.scripts.BlacklistSystem.main import handle_events as handle_Blacklist_events
 from app.scripts.WelcomeFarewell.main import (
@@ -29,19 +29,19 @@ from app.scripts.TimeAwareGreetings.main import (
     handle_events as handle_TimeAwareGreetings_events,
 )
 from app.scripts.KuaKuaAI.main import handle_events as handle_KuaKuaAI_events
-from app.scripts.PokePal.main import handle_events as handle_PokePal_events
+#from app.scripts.PokePal.main import handle_events as handle_PokePal_events
 from app.scripts.NoAddOne.main import handle_events as handle_NoAddOne_events
-from app.scripts.QFNUBustExamClassroomFind.main import (
+'''from app.scripts.QFNUBustExamClassroomFind.main import (
     handle_events as handle_QFNUBustExamClassroomFind_events,
-)
-from app.scripts.QFNUClassRegistrationCheck.main import (
+)'''
+'''from app.scripts.QFNUClassRegistrationCheck.main import (
     handle_events as handle_QFNUClassRegistrationCheck_events,
-)
+)'''
 from app.scripts.GetIPInfo.main import handle_events as handle_GetIPInfo_events
-from app.scripts.CET4.main import handle_events as handle_CET4_events
+#from app.scripts.CET4.main import handle_events as handle_CET4_events
 from app.scripts.GithubCard.main import handle_events as handle_GithubCard_events
-from app.scripts.BilibiliPush.main import handle_events as handle_BilibiliPush_events
-from app.scripts.GroupLink.main import handle_events as handle_GroupLink_events
+#from app.scripts.BilibiliPush.main import handle_events as handle_BilibiliPush_events
+#from app.scripts.GroupLink.main import handle_events as handle_GroupLink_events
 from app.scripts.QRCodeInspector.main import (
     handle_events as handle_QRCodeInspector_events,
 )
@@ -50,7 +50,7 @@ from app.scripts.TitleSelfService.main import (
 )
 from app.scripts.MuteWheel.main import handle_events as handle_MuteWheel_events
 from app.scripts.BanWords2.main import handle_events as handle_BanWords2_events
-from app.scripts.LLM.main import handle_events as handle_LLM_events
+#from app.scripts.LLM.main import handle_events as handle_LLM_events
 
 # 系统模块
 from app.system import handle_events as handle_System_events
@@ -71,11 +71,11 @@ async def handle_message(websocket, message):
         # 功能模块事件处理
         await handle_BanWords2_events(websocket, msg)
         await handle_ImageGenerate_events(websocket, msg)
-        await handle_SendAll_events(websocket, msg)
+        #await handle_SendAll_events(websocket, msg)
         await handle_GroupManager_events(websocket, msg)
-        await handle_Crypto_events(websocket, msg)
+        #await handle_Crypto_events(websocket, msg)
         await handle_QASystem_events(websocket, msg)
-        await handle_ClassTable_events(websocket, msg)
+        #await handle_ClassTable_events(websocket, msg)
         await handle_KeywordsReply_events(websocket, msg)
         await handle_Blacklist_events(websocket, msg)
         await handle_WelcomeFarewell_events(websocket, msg)
@@ -86,19 +86,19 @@ async def handle_message(websocket, message):
         await handle_Custom_events(websocket, msg)
         await handle_TimeAwareGreetings_events(websocket, msg)
         await handle_KuaKuaAI_events(websocket, msg)
-        await handle_PokePal_events(websocket, msg)
+        #await handle_PokePal_events(websocket, msg)
         await handle_NoAddOne_events(websocket, msg)
-        await handle_QFNUBustExamClassroomFind_events(websocket, msg)
-        await handle_QFNUClassRegistrationCheck_events(websocket, msg)
+        #await handle_QFNUBustExamClassroomFind_events(websocket, msg)
+        #await handle_QFNUClassRegistrationCheck_events(websocket, msg)
         await handle_GetIPInfo_events(websocket, msg)
-        await handle_CET4_events(websocket, msg)
+        #await handle_CET4_events(websocket, msg)
         await handle_GithubCard_events(websocket, msg)
-        await handle_BilibiliPush_events(websocket, msg)
-        await handle_GroupLink_events(websocket, msg)
+        #await handle_BilibiliPush_events(websocket, msg)
+        #await handle_GroupLink_events(websocket, msg)
         await handle_QRCodeInspector_events(websocket, msg)
         await handle_TitleSelfService_events(websocket, msg)
         await handle_MuteWheel_events(websocket, msg)
-        await handle_LLM_events(websocket, msg)
+        #await handle_LLM_events(websocket, msg)
 
     except Exception as e:
         logging.error(f"处理ws消息的逻辑错误: {e}")
