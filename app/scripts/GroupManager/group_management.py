db150b96abd80f2decedbe96fd58a5aa4b0cd15a
SUPER4NRNS <super4nrns@vip.qq.com>
2025-03-01 23:02:17 +0800

    banme禁言定制化改造

--- app/scripts/GroupManager/group_management.py
+++ app/scripts/GroupManager/group_management.py
@@ -46,7 +46,7 @@ async def banme_random_time(websocket, group_id, user_id, message_id):
                 websocket,
                 group_id,
                 f"[CQ:reply,id={message_id}]恭喜你打破本群今日的最高禁言记录！你抽中了 {ban_time} 秒的禁言时间，"
-                f"根据宇宙卷卷对数函数弹性计算公式实际被禁言了 {actual_ban_time} 秒。现在本群今日的最高记录是 {ban_time} 秒，保持者是{user_id}。",
+                f"根据宇宙fufu对数函数弹性计算公式实际被禁言了 {actual_ban_time} 秒。现在本群今日的最高记录是 {ban_time} 秒，保持者是{user_id}。",
             )
         else:
             max_ban_user_str = f"，保持者是{max_ban_user}" if max_ban_user else ""
@@ -58,7 +58,7 @@ async def banme_random_time(websocket, group_id, user_id, message_id):
             await send_group_msg(
                 websocket,
                 group_id,
-                f"[CQ:reply,id={message_id}]你抽中了 {ban_time} 秒的禁言时间，根据宇宙卷卷对数函数弹性计算公式实际被禁言了 {actual_ban_time} 秒。"
+                f"[CQ:reply,id={message_id}]你抽中了 {ban_time} 秒的禁言时间，根据宇宙fufu对数函数弹性计算公式实际被禁言了 {actual_ban_time} 秒。"
                 f"今日群的最高禁言记录是 {max_ban_records} 秒{max_ban_user_str}。",
             )
     except Exception as e:
