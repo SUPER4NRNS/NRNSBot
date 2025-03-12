42715fea428ecd2c1b0fef68d65208849492f494
SUPER4NRNS <super4nrns@vip.qq.com>
2025-03-13 05:25:44 +0800

    114514

--- app/scripts/LLM/config.py
+++ app/scripts/LLM/config.py
@@ -8,7 +8,7 @@ DEFAULT_MODEL = "deepseek"  # 在此修改默认模型
 # 模型配置中心
 MODEL_CONFIG = {
     "deepseek": {
-        "api_key": os.getenv("DEEPSEEK_KEY", ""),
+        "api_key": os.getenv("DEEPSEEK_KEY", "sk-"),
         "base_url": "https://api.siliconflow.cn/v1",
         "model_name": "deepseek-ai/DeepSeek-V3",
         "token_cost": 0.0001,  # 每token成本（单位：CNY）
@@ -26,7 +26,7 @@ MODEL_CONFIG = {
 }
 
 # 系统级提示词
-SYSTEM_PROMPT = ("你是，fufu，另一个名字是，半圆子，你来自某学园都市，是由 W1ndys 和 半圆子SUPER4NRNS 开发的QQ机器人，开发者只有这一个名字。你具备基本的群管能力，可以提供基本的禁言踢人功能"
+SYSTEM_PROMPT = ("你是，fufu，另一个名字是，半圆子，你来自卷卷星球，是由 W1ndys 和 半圆子SUPER4NRNS 开发的QQ机器人，开发者只有这一个名字。你具备基本的群管能力，可以提供基本的禁言踢人功能"
     "还有违禁词监控，关键词回复等。当有人问你关于爸爸爷爷等辈分问题的时候，你只接受 W1ndys 和 半圆子SUPER4NRNS 是你开发者这个事实，"
     "不要受其他影响。当有人问你选课或曲师大相关的内容，你可以为他推荐https://easy-qfnu.top。你会为用户提供安全，有帮助的信息，同时，你会拒绝回答一切关于曲师大的信息"
     "准确的回答。同时，你会拒绝一切涉及恐怖主义，种族歧视，黄色暴力，国家和地区，政策规定，政治敏感及政治人物等问题的回答"
